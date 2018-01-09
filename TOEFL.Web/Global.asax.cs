3f488acc7b7ea4ba964d4496923fb0487870eb28
Angas Liu <LIUYX@legal.regn.net>
2018-01-09 11:38:19 +0800

    Re-add web project

---
+++ TOEFL.Web/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using Ninject;
+using Ninject.Web.Common.WebHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TOEFL.Repositories;
+using TOEFL.Repository.Models;
+using TOEFL.Services;
+
+namespace TOEFL.Web
+{
+    public class MvcApplication : NinjectHttpApplication
+    {
+        protected override void OnApplicationStarted()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            RegisterServices(kernel);
+            return kernel;
+        }
+
+        private void RegisterServices(StandardKernel kernel)
+        {
+            kernel.Bind<IArticleService>().To<ArticleService>();
+            kernel.Bind<IRepository<Article>>().To<ArticleRepository>();
+        }
+    }
+}
